0ed637bc7558f603a721844430d8d1af7b587f21
Laura <keenlaura@hotmail.co.uk>
2021-02-17 16:29:36 +0000

    Enabled user to enter player names and return names as a vs. string

--- app.rb
+++ app.rb
@@ -2,9 +2,24 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/battle' do
-    'Testing infrastructure working!.'
+    erb(:index)
+  end
+
+  get '/names' do
+    @name = session[:name]
+    @name2 = session[:name2]
+    erb :names
+  end
+
+  post '/names' do
+    session[:name] = params[:Player_1]
+    session[:name2] = params[:Player_2]
+    redirect '/names'
   end
 
   run! if app_file == $0
+
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,6 +1,18 @@
-feature "Battle Homepage" do
-  scenario 'Checks for text on homepage' do
-    visit '/battle'
-    expect(page).to have_content("Testing infrastructure working!.")
+# feature "Battle Homepage" do
+#   scenario 'Checks for text on homepage' do
+#     visit '/battle'
+#     expect(page).to have_content("Testing infrastructure working!.")
+#   end
+# end
+
+feature "Allows user to enter their name" do
+  scenario "User creates new name" do
+    visit "/battle"
+
+    fill_in "Player_1", :with => "Bob"
+    fill_in "Player_2", :with => "Brian"
+    click_button "Submit"
+
+    expect(page).to have_text("Bob vs. Brian")
   end
 end
